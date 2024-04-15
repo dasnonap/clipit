fe8fbdd1aa93e98ed7937628097307c341a406c1
dasnonap <ivanmihov971@gmail.com>
2024-04-15 21:16:20 +0300

    Add media table migration

--- clip-it-web/database/migrations/2024_04_13_120451_create_media_table.php
+++ clip-it-web/database/migrations/2024_04_13_120451_create_media_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('media', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('filename', length: 64);
+
             $table->unsignedBigInteger('user_id');
             $table->timestamps();
             $table->string('path');

84769bd10041ef76486801194049e44936ead7f3
dasnonap <ivanmihov971@gmail.com>
2024-04-15 21:51:39 +0300

    Add pivot table media_tags migration

---
+++ clip-it-web/database/migrations/2024_04_15_182135_create_media_tags_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('media_tags', function (Blueprint $table) {
+            $table->foreignUuid('media_id');
+            $table->foreignId('tag_id');
+
+            $table->foreign('media_id')->references('id')->on('media');
+            $table->foreign('tag_id')->references('id')->on('tags');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('media_tags');
+    }
+};

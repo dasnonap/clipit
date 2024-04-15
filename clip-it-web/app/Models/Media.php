9c31eecc9ee9de0c80142f6a25587eea0950a337
dasnonap <ivanmihov971@gmail.com>
2024-04-15 21:52:07 +0300

    Add Media/Tags ORM connection

--- clip-it-web/app/Models/Media.php
+++ clip-it-web/app/Models/Media.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Media extends Model
 {
@@ -15,8 +17,13 @@ class Media extends Model
         'path'
     ];
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class, 'media_tags', 'media_id', 'tag_id');
     }
 }

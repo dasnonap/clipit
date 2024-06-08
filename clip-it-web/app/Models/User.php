eda65a4451b0af48d93bf9f9895ef963e05fc3a9
dasnonap <ivanmihov971@gmail.com>
2024-06-08 22:53:13 +0300

    fix(user_tests): address username property issues

--- clip-it-web/app/Models/User.php
+++ clip-it-web/app/Models/User.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+
 
 class User extends Authenticatable
 {
@@ -46,7 +48,7 @@ class User extends Authenticatable
         ];
     }
 
-    public function media()
+    public function media(): HasMany
     {
         return $this->hasMany(Media::class);
     }
